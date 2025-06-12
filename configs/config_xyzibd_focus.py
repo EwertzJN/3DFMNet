73c9b2d4d4483c27d807b11c3d4d143707271e0b
Jan Ewertz <jan_niklas.ewertz@mercedes-benz.com>
2025-06-12 09:32:06 +0200

    Added scripts to be able to train (focus) on xyz-ibd

---
+++ configs/config_xyzibd_focus.py
@@ -0,0 +1,156 @@
+import os
+import os.path as osp
+import argparse
+
+from easydict import EasyDict as edict
+from pytools import F
+
+from vision3d.utils.python_utils import ensure_dir
+
+config = edict()
+
+# random seed
+config.seed = 7351
+#scan2cad
+# dir --test_epoch 29 --benchmark 3DMatch
+
+config.xyzibd_root ='/home/jewertz/bop/xyzibd'
+
+
+
+# data
+config.voxel_size =0.0015
+config.matching_radius = config.voxel_size * 2
+#eval
+
+# train config
+config.train_batch_size = 1
+config.train_num_worker = 16
+config.train_max_num_point = 60000
+config.train_use_augmentation = True
+config.train_augmentation_noise = 0.005
+config.train_rotation_factor = 1.0
+
+# test config
+config.test_batch_size = 1
+config.test_num_worker = 16
+config.test_max_num_point = 20000
+config.test_tau1 = 0.1
+config.test_tau2 = 0.05
+config.test_registration_threshold = 0.2
+
+# optim config
+config.learning_rate = 1e-4
+config.gamma = 0.95
+config.momentum = 0.98
+config.weight_decay = 1e-6
+config.max_epoch = 200
+
+# model - KPFCNN
+config.num_layers = 4
+config.in_points_dim = 3
+config.first_feats_dim = 128
+config.final_feats_dim = 256
+config.first_subsampling_dl =config.voxel_size  #0.025   0.015 0.005 0.0015
+config.in_features_dim = 1
+config.conv_radius = 2.5#2.5 2.5
+config.deform_radius = 5.0#5.0 2.0
+config.num_kernel_points = 15
+config.KP_extent = 2.0
+config.KP_influence = 'linear'
+config.aggregation_mode = 'sum'
+config.fixed_kernel_points = 'center'
+config.normalization = 'group_norm'
+config.normalization_momentum = 0.02
+config.deformable = False
+config.modulated = False
+
+# model - Architecture
+config.architecture = ['simple', 'resnetb']
+for i in range(config.num_layers - 1):
+    config.architecture.append('resnetb_strided')
+    config.architecture.append('resnetb')
+    config.architecture.append('resnetb')
+for i in range(config.num_layers - 3):
+    config.architecture.append('nearest_upsample')
+    config.architecture.append('unary')
+config.architecture.append('nearest_upsample')
+config.architecture.append('last_unary')
+config.re_thre = 15
+config.te_thre = config.voxel_size * 4#0.01
+
+# model - Global
+config.instance_mask_thre=0.6
+config.overlap_mask_thre=0.2
+config.geodesic_radis=0.15
+config.cluster_thre=0.2
+config.cluster_refine=True
+config.ground_truth_positive_radius = config.voxel_size * 2
+config.point_to_node_max_point = 64
+
+config.sinkhorn_num_iter = 100
+config.max_ref_nodes=64
+config.max_neighboor=16#local transformer
+config.max_sample_neighboor=16 #max_sample_neighboor<max_neighboor
+config.finematch_max_point = 256#config.max_sample_neighboor*32
+config.coarse_matching_num_target=128
+config.coarse_matching_overlap_thresh = 0.1
+config.coarse_matching_num_proposal = 128
+config.coarse_matching_dual_softmax = False
+config.coarse_matching_positive_overlap = 0.
+
+config.fine_matching_max_num_corr = 15000
+config.fine_matching_min_num_corr = 30
+config.fine_matching_topk = 3
+config.fine_matching_mutual = True
+config.fine_matching_with_slack = False
+config.fine_matching_confidence_threshold = 0.1
+config.fine_matching_conditional_score = False
+config.fine_matching_positive_radius = config.voxel_size * 2
+config.fine_matching_num_registration_iter = 5
+
+# model - Coarse level
+config.coarse_tfm_feats_dim = 256
+config.coarse_tfm_num_head = 4
+# config.coarse_tfm_architecture = ['self', 'cross','mask', 'self', 'cross', 'mask', 'self', 'cross', 'mask']
+config.coarse_tfm_architecture = ['self', 'cross', 'self', 'cross', 'self', 'cross']
+config.instance_mask_architecture=[ 'cross', 'self', 'cross', 'self', 'cross','self']
+config.coarse_tfm_bin_size_d = 0.2
+config.coarse_tfm_bin_size_a = 15
+config.coarse_tfm_angle_k = 3
+
+# loss - Coarse level
+config.coarse_circle_loss_positive_margin = 0.1
+config.coarse_circle_loss_negative_margin = 1.4
+config.coarse_circle_loss_positive_optimal = 0.1
+config.coarse_circle_loss_negative_optimal = 1.4
+config.coarse_circle_loss_log_scale = 24#24
+config.coarse_circle_loss_positive_threshold = 0.1
+config.coarse_matching_pos_radius = 1.5 * config.voxel_size
+config.coarse_matching_safe_radius = 5 * config.voxel_size
+
+# loss - Fine level
+config.fine_sinkhorn_loss_positive_radius = config.voxel_size * 2
+config.dbscan_eps = config.voxel_size
+config.dbscan_min_samples = 2
+
+# loss - Overall
+config.weight_coarse_loss = 1.
+config.weight_instance_score=5.
+config.weight_center_norm_loss = 5.
+config.weight_center_dir_loss = 1.
+
+# Eval - Overall
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--link_output', dest='link_output', action='store_true', help='link output dir')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.link_output:
+        os.symlink(config.output_dir, 'output')
---
+++ lib/xyzibdFocus_trainval.py
@@ -0,0 +1,174 @@
+import argparse
+import os
+import os.path as osp
+import time
+# os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributed as dist
+import numpy as np
+from IPython import embed
+
+from vision3d.engine import Engine
+from vision3d.utils.metrics import Timer, StatisticsDictMeter
+from vision3d.utils.torch_utils import to_cuda, all_reduce_dict
+from vision3d.utils.python_utils import ensure_dir
+
+from configs.config_xyzibd_focus import config
+from utils.dataset import XYZIBD_train_data_loader
+from models.focusnet import create_model
+from models.loss import FocusLoss, Evaluator
+
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--steps', metavar='N', type=int, default=10, help='iteration steps for logging')
+    parser.add_argument('--output_dir', type=str, default='./output/xyzibd/detection_exp1')
+    parser.add_argument('--snapshot_dir', type=str, default='./output/xyzibd/detection_exp1/snapshots')
+    parser.add_argument('--logs_dir', type=str, default='./output/xyzibd/detection_exp1/logs')
+    parser.add_argument('--xyzibd_root', type=str, default='/home/jewertz/bop/xyzibd')
+    return parser
+
+
+def run_one_epoch(
+        engine,
+        epoch,
+        data_loader,
+        model,
+        evaluator,
+        loss_func=None,
+        optimizer=None,
+        scheduler=None,
+        training=True
+):
+    if training:
+        model.train()
+        if engine.distributed:
+            data_loader.sampler.set_epoch(epoch)
+    else:
+        model.eval()
+
+    timer = Timer()
+
+    num_iter_per_epoch = len(data_loader)
+    for i, data_dict in enumerate(data_loader):
+
+        data_dict = to_cuda(data_dict)
+        timer.add_prepare_time()
+
+        if training:
+            output_dict = model(data_dict)
+
+            loss_dict = loss_func(output_dict, data_dict)
+        else:
+            with torch.no_grad():
+                ref_length_c = data_dict['stack_lengths'][-1][0].item()
+                points_c = data_dict['points'][-1].detach()
+                points_m = data_dict['points'][1].detach()
+
+                ref_points_c = points_c[:ref_length_c]
+                src_points_c = points_c[ref_length_c:]
+                output_dict = model(data_dict)
+                result_dict = evaluator(output_dict, data_dict)
+                result_dict = {key: value for key, value in result_dict.items()}
+
+        if training:
+            loss = loss_dict['loss']
+
+            if engine.distributed:
+                loss_dict = all_reduce_dict(loss_dict, world_size=engine.world_size)
+            loss_dict = {key: value.item() for key, value in loss_dict.items()}
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        timer.add_process_time()
+
+        if (i + 1) % engine.args.steps == 0:
+            message = 'Epoch {}/{}, '.format(epoch + 1, config.max_epoch) + \
+                      'iter {}/{}, '.format(i + 1, num_iter_per_epoch)
+            if training:
+                message += 'loss: {:.3f}, '.format(loss_dict['loss']) + \
+                           'c_loss: {:.3f}, '.format(loss_dict['c_loss']) + \
+                           'center_norm_loss: {:.3f}, '.format(loss_dict['center_norm_loss']) + \
+                           'center_dir_loss: {:.3f}, '.format(loss_dict['center_dir_loss']) + \
+                           'instance_loss: {:.3f}, '.format(loss_dict['instance_loss'])  # + \
+                # 'center_loss: {:.3f},'.format(loss_dict['center_loss'])
+            else:
+                message += 'precision: {:.3f}, '.format(result_dict['precision']) + \
+                           'recall: {:.3f}, '.format(result_dict['recall']) + \
+                           'F1_score: {:.3f}, '.format(result_dict['F1_score'])
+
+            if training:
+                message += 'lr: {:.3e}, '.format(scheduler.get_last_lr()[0])
+            message += 'time: {:.3f}s/{:.3f}s'.format(timer.get_prepare_time(), timer.get_process_time())
+            if not training:
+                message = '[Eval] ' + message
+            engine.logger.info(message)
+
+        if training:
+            engine.step()
+        torch.cuda.empty_cache()
+
+    message = 'Epoch {}, '.format(epoch + 1)
+
+    if not training:
+        message = '[Eval] ' + message
+    engine.logger.critical(message)
+
+    if training:
+        engine.register_state(epoch=epoch)
+        if engine.local_rank == 0:
+            snapshot = osp.join(engine.args.snapshot_dir, 'epoch-{}.pth.tar'.format(epoch))
+            engine.save_snapshot(snapshot)
+        scheduler.step()
+
+
+def main():
+    parser = make_parser()
+    args = parser.parse_args()
+    config.xyzibd_root = args.xyzibd_root
+    ensure_dir(args.output_dir)
+    ensure_dir(args.snapshot_dir)
+    ensure_dir(args.logs_dir)
+    log_file = osp.join(args.logs_dir, 'train-{}.log'.format(time.strftime('%Y%m%d-%H%M%S')))
+    with Engine(log_file=log_file, default_parser=parser, seed=config.seed) as engine:
+        start_time = time.time()
+        train_loader, valid_loader, neighborhood_limits = XYZIBD_train_data_loader(engine, config)
+        loading_time = time.time() - start_time
+        message = 'Neighborhood limits: {}.'.format(neighborhood_limits)
+        engine.logger.info(message)
+        message = 'Data loader created: {:.3f}s collapsed.'.format(loading_time)
+        engine.logger.info(message)
+
+        model = create_model(config).cuda()
+        if engine.distributed:
+            local_rank = engine.local_rank
+            model = nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
+        optimizer = optim.Adam(model.parameters(),
+                               lr=config.learning_rate * engine.world_size,
+                               weight_decay=config.weight_decay)
+        loss_func = FocusLoss(config).cuda()
+        evaluator = Evaluator(config).cuda()
+
+        engine.register_state(model=model, optimizer=optimizer)
+        engine.args.snapshot = None
+        if engine.args.snapshot is not None:
+            engine.load_snapshot(engine.args.snapshot)
+
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, config.gamma, last_epoch=engine.state.epoch)
+
+        for epoch in range(engine.state.epoch + 1, config.max_epoch):
+            run_one_epoch(
+                engine, epoch, train_loader, model, evaluator, loss_func=loss_func, optimizer=optimizer,
+                scheduler=scheduler, training=True
+            )
+            run_one_epoch(
+                engine, epoch, valid_loader, model, evaluator, training=False
+            )
+
+
+if __name__ == '__main__':
+    main()
